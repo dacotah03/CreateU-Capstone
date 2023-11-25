63751fdff51ec54fc1c6b002140ed71d3fca95f1
Dacotah J <dacotahj@Brainstations-MacBook-Pro.local>
2023-11-25 17:35:12 -0500

    added mobile branch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Mobile from './Pages/Mobile/Mobile';
+
+function App() {
+  return (
+    <>
+    <Mobile/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Mobile/Mobile.js
@@ -0,0 +1,12 @@
+import logo from '../../assets/logo'
+
+const Mobile = () => {
+    return ( 
+        <>
+        <img src= {logo}/>
+        <h1>This is the mobile page</h1>
+        </>
+     );
+}
+ 
+export default Mobile;
